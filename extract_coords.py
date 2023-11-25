d9c35176aa08c45cba97bf314724f87cfc15aeb7
pizzahunter2000 <lorandszabo02@gmail.com>
2023-11-26 01:18:58 +0200

    Datasets update

--- extract_coords.py
+++ extract_coords.py
@@ -7,14 +7,21 @@ if len(sys.argv) != 2:
 
 filename = sys.argv[1]
 # Load from JSON
-geo = json.load(open(filename))
+with open(filename, 'r', encoding='utf-8') as file:
+    geo = json.load(file)
 
 for feature in geo['features']:
     coords = feature['geometry']['coordinates']
-    feature['lat'] = coords[1]
-    feature['lon'] = coords[0]
+    feature['properties']['lat'] = coords[1]
+    feature['properties']['lon'] = coords[0]
+    # Remove 'lat' and 'lon' from the top-level of each feature
+    if 'lat' in feature:
+        del feature['lat']
+    if 'lon' in feature:
+        del feature['lon']
 
 
 # Dump the GeoDataFrame to a GeoJSON file
 new_filename = filename.split(".")[0] + "-coords.geojson"
-json.dump(geo, open(new_filename, "w"))
+with open(new_filename, "w", encoding='utf-8') as file:
+    json.dump(geo, file, indent = 2)
