2c95517b523c36562c2382c1c24f1426626b70d3
Raffy27 <raffy27@pm.me>
2023-11-26 03:36:54 +0200

    I hope this works

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from streamlit_keplergl import keplergl_static
 from keplergl import KeplerGl
 import geopandas as gpd
 
-gpd.options.io_engine = "pyogrio"
-
 # Data setup
 # noise = gpd.read_file("datasets/noise-algo.geojson")
 # transport = gpd.read_file("datasets/transport-berlin.geojson")
@@ -13,18 +11,18 @@ print("Loading districts")
 districts = gpd.read_file("datasets/districts-berlin.geojson")
 # print("Loading bikes")
 # bikes = gpd.read_file("datasets/bikes-coords.geojson")
-print("Loading education")
+# print("Loading education")
 education = gpd.read_file("datasets/education-coords.geojson")
-print("Loading EV charging")
+# print("Loading EV charging")
 ev_charging = gpd.read_file("datasets/ev_charging-coords.geojson")
-print("Loading fast food")
-fast_food = gpd.read_file("datasets/fast_food-coords.geojson")
-print("Loading hospitals")
+# print("Loading fast food")
+# fast_food = gpd.read_file("datasets/fast_food-coords.geojson")
+# print("Loading hospitals")
 hospitals = gpd.read_file("datasets/hospitals-coords.geojson")
-print("Loading stations")
-stations = gpd.read_file("datasets/stations-coords.geojson")
-print("Loading waste management")
-waste_management = gpd.read_file("datasets/waste_management-coords.geojson")
+# print("Loading stations")
+# stations = gpd.read_file("datasets/stations-coords.geojson")
+# print("Loading waste management")
+# waste_management = gpd.read_file("datasets/waste_management-coords.geojson")
 
 
 map = KeplerGl(height=800)
@@ -33,15 +31,15 @@ map = KeplerGl(height=800)
 # map.add_data(data=noise, name="Noise")
 map.add_data(data=districts, name="Districts")
 # map.add_data(data=bikes, name="Bikes")
-# map.add_data(data=education, name="Education")
-# map.add_data(data=ev_charging, name="EV Charging")
+map.add_data(data=education, name="Education")
+map.add_data(data=ev_charging, name="EV Charging")
 # map.add_data(data=fast_food, name="Fast Food")
 map.add_data(data=hospitals, name="Hospitals")
 # map.add_data(data=stations, name="Stations")
 # map.add_data(data=waste_management, name="Waste Management")
 
 # Layer styles
-# map.config = json.loads(open("config.json").read())
+map.config = json.loads(open("config.json").read())
 
 # App setup
 st.set_page_config(layout="wide")
